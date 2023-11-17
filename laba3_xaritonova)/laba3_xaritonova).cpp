d0bec00acfb1bbe3e1734ceb2c4cdfa23486902c
kutab4re <alfa_no_1@list.ru>
2023-11-17 13:49:18 +0300

    44

--- laba3_xaritonova)/laba3_xaritonova).cpp
+++ laba3_xaritonova)/laba3_xaritonova).cpp
@@ -9,7 +9,43 @@
 using namespace std;
 
 
+string findMaxCommonSubsequence(const string& str1, const string& str2) {
+    int m = str1.length();
+    int n = str2.length();
 
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    int i = m;
+    int j = n;
+    string result;
+
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] == str2[j - 1]) {
+            result = str1[i - 1] + result;
+            --i;
+            --j;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1]) {
+            --i;
+        }
+        else {
+            --j;
+        }
+    }
+
+    return result;
+}
 
 
 void printMatrix(const vector<vector<int>>& matrix) {
@@ -59,7 +95,7 @@ int bag(vector<vector<int>>& a, const vector<vector<int>>& w, int k, int w_max) 
     printIncludedItems(a, w, i, j);
 
     cout << "Итоговая стоимость: " << maxUtility << endl;
-    cout << "Получившийся вес: " <<  << endl;
+    cout << "Получившийся вес: " <<  w_max << endl;
 
     return 0;
 }
@@ -199,6 +235,19 @@ int main()
         }
         else if (m == 4) {
             cout << "Алгоритм Поиск максимально совпадающей подцепочки\n";
+
+            string str1, str2;
+
+            cout << "Введите первую строку: ";
+            cin >> str1;
+
+            cout << "Введите вторую строку: ";
+            cin >> str2;
+
+            string result = findMaxCommonSubsequence(str1, str2);
+
+            cout << "Максимально совпадающая подцепочка: " << result << endl;
+
             cout << "/---------------------------------------------------------------/\n" << endl;
         }
         else if (m == 5) {
